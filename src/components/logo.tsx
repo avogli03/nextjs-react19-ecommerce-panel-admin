bdfa28d86f5a72d37c58f9861be2543b11fe3a7b
AV <ardit.vogli03@gmail.com>
2026-04-23 16:41:52 +0200

    small fixes

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,6 +12,7 @@ export function Logo() {
         alt="NextAdmin logo"
         role="presentation"
         quality={100}
+        loading="eager"
       />
 
       <Image
@@ -21,6 +22,7 @@ export function Logo() {
         alt="NextAdmin logo"
         role="presentation"
         quality={100}
+        loading="eager"
       />
     </div>
   );
